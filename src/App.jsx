f01970bf5d9d937e32620b00f5cb7bc8bc4a929c
Ryeona Sherin <ryeonasherin13@gmail.com>
2026-09-10 15:07:14 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/Skills'
+import Education from './components/Education'
+import Projects from './components/Projects'
+import Achievements from './components/Achievements'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Education />
+      <Projects />
+      <Achievements />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
